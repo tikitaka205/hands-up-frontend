d5d1505b46a8c0b4d599b987c2206a9689d6374e
Kim GyeongMin <gyeongmin951@naver.com>
2022-12-10 15:26:28 +0900

    :bulb: [ Update / Fix ] JWT토큰 전달, 로컬 스토리지 정보 저장 구조 변경, 입찰 정규식 추가
    - JWT토큰 전달
      - 로그인 정보 payload혹은 access키 값인 token이 없다면 소켓 연결 시 쿼리 파라미터에 전달을 하지 않습니다.
    - 로컬 스토리지 정보 저장 구조 변경
      - 기존에는 하나의 딕셔너리에 한 상품만 저장하여 사용했음
      - 여러개의 창의 띄워 다수의 상품을 볼 경우 데이터 유지가 되지 않는 현상 발생
      - 이중 딕셔너리 형태로 바꿔서 해결
    기존 로컬 스토리지:
    	{
    		goods_id : id,
    		buyer_id : buyer_id,
    		seller_id : seller_id
    	}
    변경 후 로컬 스토리지:
    	{
    		goods_id : {
    			buyer_id : buyer_id,
    			seller_id : seller_id
    		}
    		goods_id : {
    			buyer_id : buyer_id,
    			seller_id : seller_id
    		}
    	}
    - 입찰 정규식 포함
      - 기존에는 최고가를 로컬 스토리지에 저장을 하여 사용하려 했지만,
      - '#price'의 텍스트 숫자 정보를 가져와서 비교하여 보내주는 방법을 사용해봄.
      - 그에 따른 애로 사항은 개발자 창을 띄워 최고가에 숫자가 아닌 문자를 넣고
      - 입찰을 하면 낮은 금액으로도 입찰을 현재 최고가로 가능해졌다.
      - 이를 해결하기 위해서 현재 최고가&입찰금에 정규식 필터를 걸어줬음.
      - 또한 입찰 input창에 onput속성에 정규식과 replace로 숫자만 입력 가능케 하였음(정확한 공부 필요)

--- static/js/base.js
+++ static/js/base.js
@@ -1,9 +1,4 @@
 const hostUrl = "http://127.0.0.1:8000"
-const HANDSUP = JSON.parse(localStorage.getItem('handsup', ''))
 const payload = JSON.parse(localStorage.getItem('payload', ''))
 const DATE = new Date()
-
-
-
-
-
+const url = new URL(`${window.location.href}`);

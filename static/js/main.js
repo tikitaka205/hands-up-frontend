7e2b8fd064edb36f0ab87160c69df4d28aad79e7
Kim GyeongMin <gyeongmin951@naver.com>
2022-12-12 14:58:05 +0900

    :bulb: [ Update ] 경매 리스트 필터 기능 & UI 업데이트
    - base.js에 딕셔너리를 추가하여 ajax 통신을 하고 업데이트 되면 nowPage를 1로 초기화한다.

--- static/js/main.js
+++ static/js/main.js
@@ -25,10 +25,10 @@
             $('.featured__controls li').removeClass('active');
             $(this).addClass('active');
         });
-        if ($('.featured__filter').length > 0) {
-            var containerEl = document.querySelector('.featured__filter');
-            var mixer = mixitup(containerEl);
-        }
+        // if ($('.featured__filter').length > 0) {
+        //     var containerEl = document.querySelector('.featured__filter');
+        //     var mixer = mixitup(containerEl);
+        // }
     });
 
     /*------------------

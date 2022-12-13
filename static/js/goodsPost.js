024fab0af57d20b18bff600203689e5046b4b3c2
Kim GyeongMin <gyeongmin951@naver.com>
2022-12-13 16:08:41 +0900

    :sparkles: [ Feat ] 업로드 이미지 파일 미리 보기 & 삭제

--- static/js/goodsPost.js
+++ static/js/goodsPost.js
@@ -1,149 +1,124 @@
    
-$(document).ready(function(){
-    console.log('이미지 준비되는 실행되는 함수')
-    $('#image').on("change", addFiles)
-    let today = new Date()
-    console.log(today)
-    let year = today.getFullYear()
-    let month = today.getMonth()+1
-    let max_date = today.getDate() + 2
-    let min_date = today.getDate()
 
-    // let hours = today.getHours(); // 시
-    // let minutes = today.getMinutes();  // 분
-    // let seconds = today.getSeconds();  // 초
-
-    // console.log('시간',hours, minutes, seconds)
+console.log('이미지 준비되는 실행되는 함수')
+$('#image').on("change", addFiles)
+let today = new Date()
+console.log(today)
+let year = today.getFullYear()
+let month = today.getMonth()+1
+let max_date = today.getDate() + 2
+let min_date = today.getDate()
 
 
-    date_html=`<input class="inputdate" type ="date" id ="start" min ="${year}-${month}-${min_date}" max="${year}-${month}-${max_date}"/>
-    <input class="inputtime" id="starttime" type="time" id="appt" name="appt" required style="margin-bottom: 20px;">`
-    $('#date-time').append(date_html) 
-})
+// let hours = today.getHours(); // 시
+// let minutes = today.getMinutes();  // 분
+// let seconds = today.getSeconds();  // 초
+
+// console.log('시간',hours, minutes, seconds)
+
+
+date_html=`<input class="inputdate" type ="date" id ="start" min ="${year}-${month}-${min_date}" max="${year}-${month}-${max_date}"/>
+<input class="inputtime" id="starttime" type="time" id="appt" name="appt" required style="margin-bottom: 20px;">`
+$('#date-time').append(date_html) 
+
 
 // 가격에 콤마를 찍는 함수
-function getNumber(obj){
-    var num01;
-    var num02;
-    num01 = obj.value;
-    num02 = num01.replace(/\D/g,""); 
-    num01 = setComma(num02);
-    obj.value =  num01;
+// function getNumber(obj){
+//     var num01;
+//     var num02;
+//     num01 = obj.value;
+//     num02 = num01.replace(/\D/g,""); 
+//     num01 = setComma(num02);
+//     obj.value =  num01;
 
-    $('#test').text(num01);
- }
+//     $('#test').text(num01);
+//  }
 
- function setComma(n) {
-    var reg = /(^[+-]?\d+)(\d{3})/;
-    n += '';         
-    while (reg.test(n)) {
-       n = n.replace(reg, '$1' + ',' + '$2');
-    }         
-    return n;
- }
+//  function setComma(n) {
+//     var reg = /(^[+-]?\d+)(\d{3})/;
+//     n += '';         
+//     while (reg.test(n)) {
+//        n = n.replace(reg, '$1' + ',' + '$2');
+//     }         
+//     return n;
+//  }
 
 
 
 //document.getElementById("Date").setAttribute("max", today);
 
 // date value 현재 날짜
-document.getElementById('start').value = new Date().toISOString().substring(0,10);
-let today = new Date()
-let max_date = today.getDate() + 3
-console.log(max_date)
-document.getElementById('start').setAttribute("max", max_date)
 
-
-//현재 시간
-document.getElementById('starttime').value = new Date().toISOString().slice(11,16);
-
-
-let fileTemArr =[]
-let fileTemArrlen ;
+var fileArr = [];
 
 function addFiles(e){
-    console.log('이미지를 추가하는 함수')
-    let fileList = document.getElementById('fileList')
-    let files = e.target.files;
-    let fileArr = Array.prototype.slice.call(files);
-    console.log(fileArr)
-    let filesArrlen = fileArr.length;
-    fileTemArrlen = fileTemArr.length;
-
-    if (filesArrlen > 0){
-        for (let i=0; i < filesArrlen; i++){
-            let reader = new FileReader()
-            // reader.onload = function(data){
-            //     //이미지 태그 만들기
-            //     let imgTag = document.createElement('img')
-
-            //     imgTag.setAttribute('src', data.target.result)
-            //     imgTag.setAttribute('width', '250')
-            //     imgTag.setAttribute('height','150')
-
-            //     fileList.appendChild(imgTag)
-            // }
-            reader.readAsDataURL(fileArr[i])
+    
+    $('#swiper-wrapper').empty();
+    var files = e.target.files;
+    var filesArr = Array.prototype.slice.call(files);
+    if (fileArr.length + filesArr.length > 6){
+        return alert('최대 6장 가능합니다') // 용량 등 유효성 검사 필요
+    }
+    var index = 0;
+    filesArr.forEach(element => {
+        if(!element.type.match("image.*")){
+            alert("이미지 확장자만 가능")
+            return
         }
-        //for end
-    }else{
-        fileList.innerHtml=""
-    }
-    for(let i =0; i<filesArrlen; i++){
-        fileTemArr.push(fileArr[i])
-        // $("#fileList").append("<div>" + fileArr[i].name + "<img class = 'deletebnt' src=\"/static/image/delete.png\" onclick=\"deleteFile(event, " + (fileTemArrlen+i)+ ");\"></div>");
-        $("#fileList").append(`<div id=image_${i}>` +fileArr[i].name + "<img class = 'deletebnt' src=\"/static/images/delete.png\" onclick=\"deleteFile(event, " + i+ ");\"></div>");
-    }
+        fileArr.push(element);
+
+        var reader = new FileReader();
+        reader.onload = function(e) {
+
+            var temp = `
+                <div class="p-3 col-4 m-0" id="img_id_${index}">
+                    <div  style="position:relative">
+                        <img style="box-shadow: 0 2px 5px 0px; border-radius:10px" src="${e.target.result}" data-file="${element.name}" alt="상품이미지"/>
+                        <button style="position : absolute; border-radius : 5px; background-color: #ffda00; top : 0; right:15px; color :black; font-weight:700;" onclick="deleteImage(${index})">X</button>
+                    </div>
+                </div>
+                `
+            $('#image-wrap').append(temp)
+            index++;
+        }
+        reader.readAsDataURL(element);
+    });
 }
 
-function deleteFile(eventParam, orderParam){
-    console.log('이미지를 삭제하는 함수')
-
-    fileTemArr.splice(orderParam, 1);
-    let innerHtmlTemp =""
-    let filesArrlen = fileTemArr.length
-    for(let i =0 ; i<filesArrlen; i++){
-        innerHtmlTemp += `<div id=image_${i}>` + fileTemArr[i].name + "<img class = 'deletebnt' src=\"/static/image/delete.png\" onclick=\"deleteFile(event, " + i + ");\"></div>"  
-    }
-    $(`#image_${orderParam}`).remove()
-   
-       
-    //지우는 함수 실행시 모든 이미지가 삭제되는 문제
+function deleteImage(index){
+    console.log(fileArr)
+    fileArr.splice(index, 1);
+    $(`#img_id_${index}`).remove();
+    console.log(fileArr)
 }
 
 
 function posthandle(){
+    if(fileArr.length < 1){
+        alert('최소 하나 이상의 이미지를 포함해 주세요')
+    }
     console.log('post작성 실행')
     const title = document.getElementById('title').value
     let category = $("select[name=category]").val()
-    console.log(category)
     const dateControl = document.querySelector('input[type="date"]').value
     const timeControl = document.querySelector('input[type="time"]').value
     const content = document.getElementById('content').value
-    const auction_room = 1
-    const predict_price = document.getElementById('predict_price').value
-    const start_price = document.getElementById('start_price').value
-    const trade_room=1
+    const predict_price = document.getElementById('predictPrice').value
+    const start_price = document.getElementById('startPrice').value
+    
     let fd = new FormData()
 
-
-    for(var i=0, fileTemArrlen = fileTemArr.length; i<fileTemArrlen; i++) {
-        fd.append("files", fileTemArr[i]);
+    for(var i=0; i<fileArr.length; i++) {
+        fd.append("images", fileArr[i]);
     }
 
-    console.log(fileTemArr)
-    
-    console.log(dateControl, timeControl, auction_room, trade_room)
-
     fd.append('title', title)
-    fd.append('category', category)
-    fd.append('dateControl', dateControl)
-    fd.append('timeControl', timeControl)
     fd.append('content', content)
-    fd.append('auction_room', auction_room)
+    fd.append('category', category)
+    fd.append('start_date', dateControl)
+    fd.append('start_time', timeControl)
     fd.append('predict_price', predict_price)
     fd.append('start_price', start_price)
-    fd.append('trade_room', trade_room)
     
 
    $.ajax({
@@ -165,5 +140,3 @@ function posthandle(){
         },
     })
 }
-
-

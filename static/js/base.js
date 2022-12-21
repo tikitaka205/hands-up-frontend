372a58271630db503300fcf952f2307248798515
lucius-hyeon <lucius.hyeon@gmail.com>
2022-12-21 18:28:26 +0900

    :bulb: [ Update ] 로컬 -> 서버로 URL 변경

--- static/js/base.js
+++ static/js/base.js
@@ -1,12 +1,12 @@
-// // 서버용
-// const hostUrl = "http://backend.hands-up.co.kr"
-// const backUrl = '43.200.179.49';
-// const domain = '43.200.179.49'
+// 서버용
+const hostUrl = "http://backend.hands-up.co.kr"
+const backUrl = '43.200.179.49';
+const domain = '43.200.179.49'
 
-// 로컬용
-const hostUrl = "http://127.0.0.1:8000"
-const backUrl = '127.0.0.1:8000';
-const domain = '127.0.0.1:8000';
+// // 로컬용
+// const hostUrl = "http://127.0.0.1:8000"
+// const backUrl = '127.0.0.1:8000';
+// const domain = '127.0.0.1:8000';
 
 const token = localStorage.getItem('access')
 const payload = JSON.parse(localStorage.getItem('payload', ''))
